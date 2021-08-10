3de9e8b3f758aca7c67c43b185b84d67905a6167
cha0sCat <admin@noob.pw>
2021-08-10 21:21:44 +0800

    feat: 自定义 api baseURL

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,10 +1,13 @@
 import Vue from 'vue'
 import axios from 'axios'
 import { LocalStorage } from 'quasar'
+import { baseURL } from '../../package.json'
 
 axios.defaults.headers['Content-Type'] = "application/json"
 // 从 LocalStorage 中读取 token
 axios.defaults.headers['Authorization'] = LocalStorage.getItem('jwt-token') ? 'Bearer ' + LocalStorage.getItem('jwt-token') : ''
+// asmr.one 自定义接口域名
+axios.defaults.baseURL = baseURL;
 
 export function setAxiosHeaders (token) {
   axios.defaults.headers['Authorization'] = 'Bearer ' + token
